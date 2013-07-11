d80b968ee63eb5c127133e72a08bfff819ac38fb
Neil Weintraut <neil.weintraut@gmail.com>
2013-07-11 15:19:10 -0400

    Coordinates in PV

--- public/javascript/default.js
+++ public/javascript/default.js
@@ -1,6 +1,6 @@
 /*global Backbone, jQuery, _, L */
 
-var map = L.map('map', { center:[52.48626, -1.89042], zoom: 14});
+var map = L.map('map', { center:[37.38, -122.2], zoom: 14});
 L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    attribution: '%copy; OpenStreetMap contributors'
+    attribution: '&copy; OpenStreetMap contributors'
     }).addTo(map);

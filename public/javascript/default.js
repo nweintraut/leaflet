e2cd396e4885c7a311d46716f9cf34a164d7a957
Neil Weintraut <neil.weintraut@gmail.com>
2013-07-11 15:47:52 -0400

    Markers and popups working.

--- public/javascript/default.js
+++ public/javascript/default.js
@@ -1,6 +1,20 @@
 /*global Backbone, jQuery, _, L */
 
-var map = L.map('map', { center:[37.38, -122.2], zoom: 14});
+var neils_house = [37.38, -122.2];
+var map = L.map('map', { center: neils_house, zoom: 14});
+
 L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; OpenStreetMap contributors'
     }).addTo(map);
+L.marker(neils_house, {clickable: true})
+    .bindPopup("Neil's house").addTo(map);
+var marker = [37.377, -122.21];
+L.marker(marker, 
+    {
+        clickable: true, 
+        draggable: true, 
+        icon: L.icon({iconUrl: 'images/packtpub.png', iconAnchor: [32,32], popupAnchor: [32,0]})
+    })
+    .bindPopup("Hello custom!")
+    .addTo(map);
+map.locate({setView: true});
